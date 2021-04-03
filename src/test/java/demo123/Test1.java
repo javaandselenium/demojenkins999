074031354bc82202e798b3a0dea7dc0acfed16c0
QSP <QSP@QSP>
2021-04-03 07:45:45 +0530

    Demo

---
+++ src/test/java/demo123/Test1.java
@@ -0,0 +1,11 @@
+package demo123;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+public void demo() {
+	Reporter.log("hello selenium",true);
+}
+}
---
+++ src/test/java/demo123/Test2.java
@@ -0,0 +1,11 @@
+package demo123;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+@Test
+public void demo() {
+	Reporter.log("hello python",true);
+}
+}
