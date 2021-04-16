e2775b18eb61de4dc5214e8d1b237b345cfc2588
QSP <QSP@QSP>
2021-04-16 17:24:28 +0530

    Hi

---
+++ src/test/java/demo123/Test4.java
@@ -0,0 +1,12 @@
+package demo123;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 {
+	@Test
+	public void demo() {
+		Reporter.log("hello python",true);
+	}
+
+}
